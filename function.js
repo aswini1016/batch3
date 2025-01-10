7c1aa6549e20015a515e7d3aa402bfcbc99dbadc
aswini1016 <aswinibumika@gmail.com>
2025-01-10 10:35:09 +0530

    demo3

---
+++ function.js
@@ -0,0 +1,11 @@
+function demo(a,b){
+    a+b;
+    };
+    let x = demo(5,5);
+    console.log(x);
+
+function hof(a){
+    return a;
+};
+let x = hof("Ashwini");
+console.log(x);
---
+++ nested.js
@@ -0,0 +1,19 @@
+var a = 100;
+let b = "Hi";
+function x(){
+    var user = "Ashwini";
+    let company = "Google";
+    const sal = 1234456789;
+    console.loh(user);
+    console.log(company);
+    console.log(sai);
+    console.log(a,b);
+};
+x();
+
+for(let i=1;i<=5;i++){
+    FocusEvent(let j=1;j<=5;j++){
+        document.writeln("*")
+    }
+    document.write("<br>")
+}
