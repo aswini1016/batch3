29399a19bc6092069c8f769901a66dba3f2fabc2
aswini1016 <aswinibumika@gmail.com>
2025-01-22 14:02:19 +0530

    day11

---
+++ fetch.js
@@ -0,0 +1,17 @@
+function fetchUsers() {
+    fetch("https://jsonplaceholder.typicode.com/users")  // Fetch data from the URL
+      .then(response => {
+        // Parse the response as JSON
+        return response.json().then(data => {
+          // Log the parsed data
+          console.log(data);
+        });
+      })
+      .catch(err => {
+        // Catch and log any errors
+        console.log(err);
+      });
+  }
+  
+  fetchUsers();
+  
